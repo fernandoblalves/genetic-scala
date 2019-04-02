e6724efcd4e21edcb7e2e3f0d4720cd3fdfcf909
fernando <fernandoblalves@gmail.com>
2019-04-02 15:20:22 +0100

    small changes

--- src/main/scala/genetic/EntryPoint.java
+++ src/main/scala/genetic/EntryPoint.java
@@ -5,6 +5,6 @@ public class EntryPoint {
 	public static void main(String[] args){
 		GeneticEvolver ge = new GeneticEvolver(100, 100, 100);
 		Evaluator evaluator = new NetworkEvaluator();
-		ge.run(evaluator);
+		Organism best = ge.run(evaluator);
 	}
 }
--- src/main/scala/genetic/TestEvaluator.scala
+++ src/main/scala/genetic/TestEvaluator.scala
@@ -2,9 +2,5 @@ package genetic
 
 class TestEvaluator extends Evaluator {
 
-	def fitness(organism: Organism): Double = {
-		val score: Double = organism.chromosome.map(_.toInt).sum
-
-		score / organism.chromosome.length
-	}
+	def fitness(organism: Organism): Double = organism.chromosome.map(_.toInt).sum / organism.chromosome.length
 }
